40cd9d984ccecbfb0501a2abf4a2d976494fc383
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-09-17 00:55:54 +0900

    new: 백준 1697

---
+++ 0x09_BFS/5_1697_hide.kt
@@ -0,0 +1,40 @@
+package `kotlin-algorithm`.`0x09_BFS`
+
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (s, b) = readLine().split(" ").map { it.toInt() }
+    val distance = IntArray(100001)
+    val dx = arrayOf(-1, 1, 2)
+
+    val bfs = bfs@ { x: Int ->
+        val que = LinkedList<Int>()
+        que.offer(x)
+        distance[x] = 1
+
+        while (que.isNotEmpty()) {
+            val a = que.poll()
+
+            if (a == b) return@bfs
+
+            for (i in dx.indices) {
+                val a1 = if (i == dx.lastIndex) a * dx[i] else a + dx[i]
+
+//                if (a1 == b) {
+//                    distance[a1] = distance[a] + 1
+//                    return@bfs
+//                }
+
+                if (a1 < 0 || a1 >= distance.size || distance[a1] != 0) continue
+
+                distance[a1] = distance[a] + 1
+                que.offer(a1)
+            }
+        }
+    }
+
+    bfs(s)
+
+    println(distance[b] - 1)
+
+}
