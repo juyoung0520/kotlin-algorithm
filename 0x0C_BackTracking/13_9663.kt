001d3ab5513c0b7de53bea9cddbad6b03517b9e7
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-09-30 14:22:38 +0900

    new: 백준 9663

---
+++ 0x0C_BackTracking/13_9663.kt
@@ -0,0 +1,46 @@
+package `kotlin-algorithm`.`0x0C_BackTracking`
+
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
+private lateinit var visited: Array<BooleanArray>
+private lateinit var array: IntArray
+private var count = 0
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    visited = Array(n) { BooleanArray(n) }
+    array = IntArray(n)
+
+    dfs(0, n)
+
+    println(count)
+}
+
+private fun dfs(idx: Int, n: Int) {
+    if (idx == n) {
+        count++
+        return
+    }
+
+    // idx는 행, 반복문으로 열 탐색
+    for (i in 0 until n) {
+        if (isPossibleColumn(idx, i)) {
+            // idx 행에서 i 열 선택
+            array[idx] = i
+            dfs(idx + 1, n)
+        }
+    }
+}
+
+private fun isPossibleColumn(row: Int, column: Int): Boolean {
+    for (i in 0 until row) {
+        if (array[i] == column) {
+            return false
+        } else if (abs(array[i] - column) == abs(i - row)) {
+            return false
+        }
+    }
+
+    return true
+}
