0c762cef178f3d86fd4ad5c3d1a0cf368d742dcf
Juyoung Jin <54929665+juyoung0520@users.noreply.github.com>
2022-11-13 02:12:13 +0900

    Create 4_2295_세수의합.kt
    new: 백준 2295 세 수의 합

---
+++ 0x13_BinarySearch/4_2295_세수의합.kt
@@ -0,0 +1,56 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val arr = IntArray(n) {
+        br.readLine().toInt()
+    }
+
+    arr.sort()
+
+    val sumList = mutableListOf<Int>() // x + y = k - z 에서 x + y 에 대한 리스트
+    for (i in arr.indices) {
+        for (j in i until arr.size) {
+            sumList.add(arr[i] + arr[j])
+        }
+    }
+
+    sumList.sort()
+
+    // k번째 수가 최대가 되도록이므로 뒤에서 부터 탐색
+    for (i in arr.lastIndex downTo 0) {
+        for (j in 0 until i) {
+            // k - z 에 해당하는 x + y가 있는지 탐색
+            if (sumList.binarySearch(arr[i] - arr[j]) >= 0) {
+                println(arr[i])
+                return
+            }
+        }
+    }
+}
+
+private fun search(sumList: List<Int>, find: Int): Boolean {
+    var start = 0
+    var end = sumList.lastIndex
+    var mid: Int
+
+    while (start <= end) {
+        mid = (start + end) / 2
+
+        when {
+            find == sumList[mid] -> {
+                return true
+            }
+            find < sumList[mid] -> {
+                end = mid - 1
+            }
+            else -> {
+                start = mid + 1
+            }
+        }
+    }
+
+    return false
+}
