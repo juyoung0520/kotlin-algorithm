af316172a0592c529c46ff4f8f0caccea17e4afa
Juyoung Jin <jinjuyoung05207@gmail.com>
2023-04-12 16:17:59 +0900

    add: 백준 7453 합이0인네정수

---
+++ 0x13_BinarySearch/18_7453_합이0인네정수.kt
@@ -0,0 +1,83 @@
+package `kotlin-algorithm`.`0x13_BinarySearch`
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+private lateinit var arr: Array<IntArray>
+private lateinit var sum1: IntArray
+private lateinit var sum2: IntArray
+
+fun main() {
+    println(System.currentTimeMillis())
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    arr = Array(4) {
+        IntArray(n)
+    }
+
+    var st: StringTokenizer
+    for (i in 0 until n) {
+        st = StringTokenizer(br.readLine())
+        for (j in arr.indices) {
+            arr[j][i] = st.nextToken().toInt()
+        }
+    }
+
+    sum1 = IntArray(n * n)
+    sum2 = IntArray(n * n)
+    var idx = 0
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            sum1[idx] = arr[0][i] + arr[1][j]
+            sum2[idx++] = arr[2][i] + arr[3][j]
+        }
+    }
+
+    sum1.sort() // 캐시 시간적 지연성 때문에 하는게 더 빠르다.
+    sum2.sort()
+
+    var count = 0L
+    for (i in sum1.indices) {
+        count += (upperBound(-sum1[i]) - lowerBound(-sum1[i])).toLong() // 여러 개 일 수 있다.
+    }
+
+    println(count)
+    println(System.currentTimeMillis())
+}
+
+private fun upperBound(find: Int): Int {
+    var s = 0
+    var e = sum2.size
+    var mid = 0
+
+    while (s < e) {
+        mid = (s + e) / 2
+
+        if (sum2[mid] <= find) {
+            s = mid + 1
+        } else {
+            e = mid
+        }
+    }
+
+    return s
+}
+
+private fun lowerBound(find: Int): Int {
+    var s = 0
+    var e = sum2.size
+    var mid = 0
+
+    while (s < e) {
+        mid = (s + e) / 2
+
+        if (sum2[mid] < find) {
+            s = mid + 1
+        } else {
+            e = mid
+        }
+    }
+
+    return s
+}
