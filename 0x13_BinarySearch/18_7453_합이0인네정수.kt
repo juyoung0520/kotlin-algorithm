cb9900121d6803ce7bea6c60886d8b5b165f32c5
Juyoung Jin <jinjuyoung05207@gmail.com>
2023-04-13 17:18:37 +0900

    fix: 백준 7453 합이0인네정수

--- 0x13_BinarySearch/18_7453_합이0인네정수.kt
+++ 0x13_BinarySearch/18_7453_합이0인네정수.kt
@@ -9,7 +9,6 @@ private lateinit var sum1: IntArray
 private lateinit var sum2: IntArray
 
 fun main() {
-    println(System.currentTimeMillis())
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     arr = Array(4) {
@@ -37,13 +36,13 @@ fun main() {
     sum1.sort() // 캐시 시간적 지연성 때문에 하는게 더 빠르다.
     sum2.sort()
 
-    var count = 0L
-    for (i in sum1.indices) {
-        count += (upperBound(-sum1[i]) - lowerBound(-sum1[i])).toLong() // 여러 개 일 수 있다.
-    }
-
-    println(count)
-    println(System.currentTimeMillis())
+//    var count = 0L
+//    for (i in sum1.indices) {
+//        count += (upperBound(-sum1[i]) - lowerBound(-sum1[i])).toLong() // 여러 개 일 수 있다.
+//    }
+//
+//    println(count)
+    println(getZeroCount())
 }
 
 private fun upperBound(find: Int): Int {
@@ -80,4 +79,42 @@ private fun lowerBound(find: Int): Int {
     }
 
     return s
+}
+
+// two pointer 방식, 여기서는 ab 배열, cd 배열 모두 sort 해야 됨
+private fun getZeroCount(): Long {
+    var p1 = 0
+    var p2 = sum2.size - 1
+    var count = 0L
+    var sum = 0
+
+    while (p1 < sum1.size && p2 >= 0) {
+        sum = sum1[p1] + sum2[p2]
+        when {
+            sum == 0 -> {
+                var c1 = 0L
+                var find = sum1[p1]
+                while (p1 < sum1.size && sum1[p1] == find) {
+                    c1++
+                    p1++
+                }
+
+                var c2 = 0L
+                find = sum2[p2]
+                while (p2 >= 0 && sum2[p2] == find) {
+                    c2++
+                    p2--
+                }
+                count += c1 * c2
+            }
+            sum < 0 -> {
+                p1++
+            }
+            else -> {
+                p2--
+            }
+
+        }
+    }
+    return count
 }
