4f2aedaf7af337fb2bd2219c0339b86d07eea477
Juyoung Jin <jinjuyoung05207@gmail.com>
2023-07-31 13:09:33 +0900

    add: 백준 2293 동전1

--- 0x10_DP/28_2293_동전1.kt
+++ 0x10_DP/28_2293_동전1.kt
@@ -2,32 +2,42 @@ package `kotlin-algorithm`.`0x10_DP`
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.util.StringTokenizer
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val tc = br.readLine().toInt()
-    val sb = StringBuilder()
-    repeat(tc) {
-        val n = br.readLine().toInt()
-        val st = StringTokenizer(br.readLine())
-        val coins = IntArray(n) {
-            st.nextToken().toInt()
-        }
-        val m = br.readLine().toInt()
-        val dp = IntArray(m + 1)
-
-        for (i in coins.indices) {
-            if (coins[i] < dp.size) {
-                dp[coins[i]]++
-            }
-            for (j in coins[i] + 1 until dp.size) {
-                dp[j] += dp[j - coins[i]]
-            }
-        }
-        sb.appendLine(dp[m])
+    val st = StringTokenizer(br.readLine())
+    val n = st.nextToken().toInt()
+    val k = st.nextToken().toInt()
+    val coins = IntArray(n) {
+        br.readLine().toInt()
     }
 
-    println(sb.toString())
+    val dp = Array(n) {
+        IntArray(k + 1) { j ->
+            if (j == 0) {
+                1
+            } else {
+                0
+            }
+        }
+    }
+
+    for (i in 1 until dp[0].size) {
+        if (i >= coins[0] && i % coins[0] == 0) {
+            dp[0][i] = 1
+        }
+    }
+
+    for (i in 1 until dp.size) {
+        for (j in 1 until dp[0].size) {
+            if (j < coins[i]) {
+                dp[i][j] = dp[i - 1][j] // 이전 조합에서 현재 값을 만드는 모든 경우
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]] // 이전 조합과 현재 조합에서 현재 값을 만드는 모든 경우
+            }
+        }
+    }
+
+    println(dp[n - 1][k])
 }
